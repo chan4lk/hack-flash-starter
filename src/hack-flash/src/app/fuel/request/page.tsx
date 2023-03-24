db21185dd1ec049d03b26cc63adfb7c48220c681
Chandima Ranaweera <chandima@bistecglobal.com>
2023-03-24 15:11:00 +0530

    trigger build

--- src/hack-flash/src/app/fuel/request/page.tsx
+++ src/hack-flash/src/app/fuel/request/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
     <main className={styles.main}>
       <div className={styles.clHeader}>Bistec Fuel Pass</div>
       <div className={styles.clWrapper}>
-        Not Implemented
+        
       </div>
     </main>
   );
